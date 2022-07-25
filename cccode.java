4b56db38aaf8f6eb7281af77f17f537642d9ba80
Sangeekayal05 <98104385+Sangeekayal05@users.noreply.github.com>
2022-07-25 14:14:47 +0530

    Add files via upload

---
+++ cccode.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class cccode{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter n:");
+int n=sc.nextInt();
+int i;
+for(i=0;i<n;i++){
+ if(i%2==0){
+ System.out.println(i+" ");
+}
+}
+}
+}
