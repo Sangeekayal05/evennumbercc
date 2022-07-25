0f623d5f718abf931a4545eec91900b92a893bbb
ShakthiCG <cgshakthi@gmail.com>
2022-07-25 14:52:56 +0530

    made some changes

--- cccode.java
+++ cccode.java
@@ -4,11 +4,11 @@ public static void main(String args[]){
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter n:");
 int n=sc.nextInt();
-int i;
-for(i=0;i<n;i++){
+System.out.println("Even numbers from 1 to n : ");
+for(int i=0;i<n;i++){
  if(i%2==0){
- System.out.println(i+" ");
+ System.out.print(i+" ");
 }
-    }
-    }
-        }
+}
+}
+}
